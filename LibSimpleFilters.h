6d54ff90df8fc56cbce66f67a386a57b62b6b2f2
Adam Cooper <arc@hilltop-cottage.info>
2012-08-27 19:46:40 +0100

    convenience header

---
+++ LibSimpleFilters.h
@@ -0,0 +1,6 @@
+//This is just a convenience "Header" to include all filter classes in the library
+#include "./MovingAverage.h"
+#include "./SimpleLowPass.h"
+#include "./SimpleHighPass.h"
+#include "./MedianFilter.h"
+#include "./ButterworthLowPass2.h"
